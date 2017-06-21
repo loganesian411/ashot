596f0130d7e7ffa458702fa6f747c98758db1782
loganesian411 <loganesian411@gmail.com>
2017-06-20 20:47:50 -0700

    removed storage

--- storage/framework/views/210ad0adf49720da0c2518e336ee3b3d38b41072.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('header'); ?>
-<div class="header-content">
-  <div class="header-content-inner">
-	<h3 align="center">Thank you for getting in touch! I'll get back to you as soon as possible</h3>
-	<p align="center"><a href="/">Return to the homepage.</a></p>
-  </div>
-</div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.base', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/801f49a94e565a17911534dd03db3c722f684248.php
+++
@@ -1,6 +0,0 @@
-<div id="navbar-custom">
-    <ul class="navlist">
-      <li><a href="/">About</a></li>
-      <li><a href="/gallery">Gallery</a></li>
-    </ul>
-</div>
